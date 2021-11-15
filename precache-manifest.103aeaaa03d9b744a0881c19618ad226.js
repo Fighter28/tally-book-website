e1fbe8e20cb37b84727b5f9d666c95a185ec49d7
Fighter28 <zhengzhinan28@gmail.com>
2021-11-15 13:41:54 +0800

    deploy

---
+++ precache-manifest.103aeaaa03d9b744a0881c19618ad226.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecd45081b4b2766bce3e",
+    "url": "/tally-book-website/css/app.201ada77.css"
+  },
+  {
+    "revision": "719f124d50938763cbb7eb7c68060aca",
+    "url": "/tally-book-website/index.html"
+  },
+  {
+    "revision": "ecd45081b4b2766bce3e",
+    "url": "/tally-book-website/js/app.7bb6d694.js"
+  },
+  {
+    "revision": "dc279e4107d98c318daf",
+    "url": "/tally-book-website/js/chunk-vendors.e3f4116a.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/tally-book-website/manifest.json"
+  },
+  {
+    "revision": "b2ac8b3d87e9ba324261cb50c7d1ad5a",
+    "url": "/tally-book-website/qrcodes.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tally-book-website/robots.txt"
+  }
+]);
